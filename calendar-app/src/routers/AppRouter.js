61d16785d97465ed5b669d8a63622c87ceb5445f
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-03 21:40:56 -0500

    Inicio Proyecto

---
+++ calendar-app/src/routers/AppRouter.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from "react-router-dom";
+import { PublicRoute } from './PublicRoute';
+import { PrivateRoute } from './PrivateRoute';
+import { AuthRouter } from './AuthRouter';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+import { LoginScreen } from '../components/auth/LoginScreen';
+
+export const AppRouter = () => {
+    return (
+        <Router>
+            <div>
+                <Switch>
+
+                    <Route exact path="/login" component={ LoginScreen } />
+                    <Route exact path="/register" component={ RegisterScreen } />
+                    <Route exact path="/" component={ CalendarScreen } />
+                    <Redirect to="/" />
+
+                    {/*
+                        <PublicRoute 
+                        path="/auth" 
+                        component={ AuthRouter }
+                        // isAuthenticated={ isLoggedIn }
+                        />
+                        
+                        <PrivateRoute 
+                            exact
+                            path="/" 
+                            component={ CalendarScreen }
+                            // isAuthenticated={ isLoggedIn }
+                        />
+
+                        <Redirect to="/auth/login" />
+                    */}
+                    
+                </Switch>
+            </div>
+        </Router>
+    );
+};
