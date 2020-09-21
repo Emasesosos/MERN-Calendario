2a75664b00f0fd53543da39414bd25a598671cd3
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-09-20 19:23:58 -0500

    AuthReducer

--- calendar-app/src/redux/types/types.js
+++ calendar-app/src/redux/types/types.js
@@ -7,6 +7,15 @@ export const types = {
     eventAddNew: '[Event] Add New',
     eventClearActiveEvent: '[Event] Clear Active Event',
     eventUpdated: '[Event] Event Updated',
-    eventDeleted: '[Event] Event Deleted'
+    eventDeleted: '[Event] Event Deleted',
+
+    authChecking: '[Auth] Checking Login State',
+    authCheckingFinish: '[Auth] Finish Checking Login State',
+    authStartLogin: '[Auth] Start Login',
+    authLogin: '[Auth] Login',
+    authStartRegister: '[Auth] Start Register',
+    authStartTokenRenew: '[Auth] Start Token Renew',
+    authLogout: '[Auth] Logout',
+    authRestablishPass: '[Auth] Restablish Pass'
 
 };
