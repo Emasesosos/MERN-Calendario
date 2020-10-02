8c3897028683956498196d2489b5ac97c25abe0f
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-10-01 20:41:24 -0500

    Pruebas con el registro de usuarios

--- calendar-app/src/tests/redux/actions/auth.test.js
+++ calendar-app/src/tests/redux/actions/auth.test.js
@@ -2,8 +2,9 @@ import '@testing-library/jest-dom';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import Swal from 'sweetalert2';
-import { startLogin } from '../../../redux/actions/auth';
+import { startLogin, startRegister } from '../../../redux/actions/auth';
 import { types } from '../../../redux/types/types';
+import * as fetchModule from './../../../helpers/fetch';
 
 jest.mock('sweetalert2', () => {
     return {
@@ -53,16 +54,47 @@ describe('Pruebas en las acciones del Auth', () => {
 
         await store.dispatch(startLogin('emasesosos@gmail.com', '123455'));
         let actions = store.getActions();
-        console.log(actions);
+        //  console.log(actions);
         expect(actions).toEqual([]);
         expect(Swal.fire).toHaveBeenCalledWith("Error", "Password incorrecto", "error");
 
         await store.dispatch(startLogin('emasesosos2@gmail.com', '123456'));
         actions = store.getActions();
-        console.log(actions);
+        //  console.log(actions);
         expect(Swal.fire).toHaveBeenCalledWith("Error", "El usuario no existe con ese email", "error");
 
     });
 
+    test('startRegister correcto', async() => {
+
+        fetchModule.fetchSinToken = jest.fn(() => {
+            return {
+                json() {
+                    return {
+                        ok: true,
+                        uid: '123',
+                        name: 'casandra',
+                        token: 'ABC123ABC123'
+                    }
+                }
+            }
+        });
+
+        await store.dispatch(startRegister('Test', 'test@test.com', '123456'));
+        const actions = store.getActions();
+        //  console.log(actions);
+
+        expect(actions[0]).toEqual({
+            type: types.authLogin,
+            payload: {
+                uid: '123',
+                name: 'casandra'
+            }
+        });
+
+        expect(localStorage.setItem).toHaveBeenCalledWith('token', 'ABC123ABC123');
+        expect(localStorage.setItem).toHaveBeenCalledWith('token-init-date', expect.any(Number));
+
+    });
 
 });
