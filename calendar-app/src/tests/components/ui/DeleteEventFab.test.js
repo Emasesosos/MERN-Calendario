66cf1d9a5aeaa80ae094b73608884db026adb5d6
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-10-06 19:59:37 -0500

    Prueba disparando la acción eventStartDelete

--- calendar-app/src/tests/components/ui/DeleteEventFab.test.js
+++ calendar-app/src/tests/components/ui/DeleteEventFab.test.js
@@ -5,6 +5,13 @@ import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { DeleteEventFab } from '../../../components/ui/DeleteEventFab';
+import { eventStartDelete } from '../../../redux/actions/events';
+
+jest.mock('./../../../redux/actions/events.js', () => {
+    return {
+        eventStartDelete: jest.fn()
+    }
+})
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -24,5 +31,14 @@ describe('Pruebas en <DeleteEventFab />', () => {
     test('Debe de mostrarse correctamente', () => {
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('Debe de llamar el eventStartDelete al hacer el click', () => {
+        
+        wrapper.find('button').prop('onClick')();
+
+        expect(eventStartDelete).toHaveBeenCalled();
+
+    });
+    
 
 });
