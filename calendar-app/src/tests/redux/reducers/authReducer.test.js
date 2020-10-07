cd57615454c6a9892bed52632bda5fd27f38753b
Emmanuel Martínez <j.emmanuel.martinez.e@hotmail.com>
2020-10-06 19:40:30 -0500

    Pruebas en el authReducer

--- calendar-app/src/tests/redux/reducers/authReducer.test.js
+++ calendar-app/src/tests/redux/reducers/authReducer.test.js
@@ -1,5 +1,6 @@
 import '@testing-library/jest-dom';
 import { authReducer } from '../../../redux/reducers/authReducer';
+import { types } from '../../../redux/types/types';
 
 const initState = {
     checking: true,
@@ -11,9 +12,27 @@ describe('Pruebas en authReducer', () => {
 
     test('Debe de retornar el estado por defecto', () => {
 
-        const state = authReducer(initState, {});
+        const action = {};
+        const state = authReducer(initState, action);
         // console.log(state);
         expect(state).toEqual(initState);
+
+    });
+
+    test('Debe de autenticar el usuario', () => {
+
+        const action = {
+            type: types.authLogin,
+            payload: {
+                uid: 123,
+                name: 'Emmanuel'
+            }
+        };
+
+        const state = authReducer(initState, action);
+        // console.log(state);
+
+        expect(state).toEqual({ checking: false, uid: 123, name: 'Emmanuel' });
 
     });
 
